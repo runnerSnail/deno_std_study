3eedefd005b46a9397430ca5bdb314b563dcf84d
高铨康 <hzgaoquankang@corp.netease.com>
2019-04-24 15:15:33 +0800

    修改代码

--- test.ts
+++ test.ts
@@ -1,3 +1,1 @@
-#!/usr/bin/env deno -A
-// Copyright 2018-2019 the Deno authors. All rights reserved. MIT license.
-import "./http_compose/application_test.ts";
+import "./http_compose/onion_test.ts";
